8cc5400f104e0ffd58f79a4b6955cb1496cbeeb5
Nazmul66 <hnazmul748@gmail.com>
2025-09-19 01:37:39 +0600

    Some bug fix issue

--- resources/views/admin/pages/department/index.blade.php
+++ resources/views/admin/pages/department/index.blade.php
@@ -66,20 +66,25 @@
                                         <div class="dropdown">
                                             <a href="#" class="action-icon border-0" data-bs-toggle="dropdown" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical feather-user"><circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="5" r="1"></circle><circle cx="12" cy="19" r="1"></circle></svg></a>
                                             <ul class="dropdown-menu dropdown-menu-end" style="">
-                                                <li>
-                                                    <a href="javascript:void(0);" class="dropdown-item" 
-                                                    id="editButton"
-                                                    data-id="{{ $row->id }}"
-                                                    data-bs-toggle="modal" data-bs-target="#editModal">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit info-img me-2"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>Edit</a>
-                                                </li>
-                                                <li>
-                                                    <a href="javascript:void(0);" class="dropdown-item mb-0" 
-                                                    id="deleteBtn" 
-                                                    data-id="{{ $row->id }}" 
-                                                    >
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 info-img me-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>Delete</a>
-                                                </li>								
+                                                @if(auth("admin")->user()->can("update.department"))
+                                                    <li>
+                                                        <a href="javascript:void(0);" class="dropdown-item" 
+                                                        id="editButton"
+                                                        data-id="{{ $row->id }}"
+                                                        data-bs-toggle="modal" data-bs-target="#editModal">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit info-img me-2"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>Edit</a>
+                                                    </li>
+                                                @endif
+
+                                                @if(auth("admin")->user()->can("delete.department"))
+                                                    <li>
+                                                        <a href="javascript:void(0);" class="dropdown-item mb-0" 
+                                                        id="deleteBtn" 
+                                                        data-id="{{ $row->id }}" 
+                                                        >
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 info-img me-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>Delete</a>
+                                                    </li>	
+                                                @endif							
                                             </ul>
                                         </div>
                                     </div>
